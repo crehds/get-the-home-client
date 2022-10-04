725f9fef149ff43f31a6adfa0b69ff7e09d502aa
Carlos Rodriguez <carlos15erh@gmail.com>
2022-10-04 12:27:40 -0500

    Feat: dark mode to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,21 @@
+import { themes } from '@storybook/theming';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
-      date: /Date$/,
-    },
+      date: /Date$/
+    }
   },
-}
+  darkMode: {
+    // Override the default dark theme
+    dark: { ...themes.dark, appBg: 'black' },
+    // Override the default light theme
+    light: { ...themes.normal, appBg: 'white' },
+    darkClass: 'lights-out',
+    lightClass: 'lights-on'
+  },
+  layout: 'centered',
+  margin: 0
+};
