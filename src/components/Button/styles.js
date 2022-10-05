b985b237a9753712cda0dea7164e57b601a0337f
Carlos Rodriguez <carlos15erh@gmail.com>
2022-10-04 22:04:18 -0500

    Add: buttons v1

---
+++ src/components/Button/styles.js
@@ -0,0 +1,104 @@
+import styled from '@emotion/styled';
+import { colors } from '../../styles/colors';
+
+export const SmallButton = styled.button`
+  display: flex;
+  gap: 0.8rem;
+  border-radius: 0.8rem;
+  padding: 0.4rem 0.8rem;
+  color: ${colors.gray};
+  &.primary {
+    background-color: ${colors.pink};
+    color: ${colors.white};
+    &:hover {
+      background-color: ${colors.darkPink};
+    }
+  }
+  &.secondary {
+    border: 1px solid ${colors.pink};
+    &:hover {
+      color: ${colors.darkGray};
+      border: 1px solid ${colors.darkPink};
+      background-color: ${colors.shallowPink};
+    }
+  }
+  &.ghost {
+    color: ${colors.gray};
+    &:hover {
+      background-color: ${colors.shallowPink};
+      color: ${colors.darkGray};
+    }
+  }
+  &:disabled {
+    color: ${colors.shallowGray};
+    background-color: ${colors.lightGray};
+  }
+`;
+
+export const DefaultButton = styled.button`
+  border-radius: 1.6rem;
+  display: flex;
+  gap: 0.8rem;
+  padding: 0.8rem 1.6rem;
+  color: ${colors.gray};
+  &.primary {
+    background-color: ${colors.pink};
+    color: ${colors.white};
+    &:hover {
+      background-color: ${colors.darkPink};
+    }
+  }
+  &.secondary {
+    border: 1px solid ${colors.pink};
+    &:hover {
+      color: ${colors.darkGray};
+      border: 1px solid ${colors.darkPink};
+      background-color: ${colors.shallowPink};
+    }
+  }
+  &.ghost {
+    color: ${colors.gray};
+    &:hover {
+      background-color: ${colors.shallowPink};
+      color: ${colors.darkGray};
+    }
+  }
+  &:disabled {
+    color: ${colors.shallowGray};
+    background-color: ${colors.lightGray};
+  }
+`;
+
+export const LargeButton = styled.button`
+  border-radius: 1.6rem;
+  display: flex;
+  gap: 0.8rem;
+  color: ${colors.gray};
+  padding: 1.6rem 2.4rem;
+  &.primary {
+    background-color: ${colors.pink};
+    color: ${colors.white};
+    &:hover {
+      background-color: ${colors.darkPink};
+    }
+  }
+  &.secondary {
+    border: 1px solid ${colors.pink};
+    &:hover {
+      color: ${colors.darkGray};
+      border: 1px solid ${colors.darkPink};
+      background-color: ${colors.shallowPink};
+    }
+  }
+  &.ghost {
+    color: ${colors.gray};
+    &:hover {
+      background-color: ${colors.shallowPink};
+      color: ${colors.darkGray};
+    }
+  }
+  &:disabled {
+    color: ${colors.shallowGray};
+    background-color: ${colors.lightGray};
+  }
+`;
