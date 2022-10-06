a40b633034b37f9662e4a6f7cc22e7a78936fa09
Carlos Rodriguez <carlos15erh@gmail.com>
2022-10-06 04:14:03 -0500

    Update Select styles

--- src/components/Select/styles.js
+++ src/components/Select/styles.js
@@ -2,13 +2,37 @@ import styled from '@emotion/styled';
 import { colors } from '../../styles/colors';
 import { typography } from '../../styles/typography';
 
+export const SelectWrapper = styled.div`
+  display: flex;
+  position: relative;
+  width: 100%;
+`;
+
 export const SelectLabel = styled.label`
   ${typography.overline};
   color: ${colors.gray};
 `;
 
 export const colourStyles = {
-  control: (styles) => ({ ...styles, backgroundColor: colors.white }),
+  container: (styles) => ({
+    ...styles,
+    width: '100%',
+    '&:active': {
+      borderColor: 'green'
+    }
+  }),
+  control: (styles, { isSelected }) => ({
+    ...styles,
+    color: isSelected ? colors.lightGray : colors.darkGray,
+    borderColor: colors.pink,
+    boxShadow: 'none',
+    '&:hover': {
+      borderColor: colors.darkPink
+    },
+    '&:active': {
+      borderColor: colors.darkPink
+    }
+  }),
   option: (styles, { data, isDisabled, isFocused, isSelected }) => {
     return {
       ...styles,
@@ -37,14 +61,17 @@ export const colourStyles = {
       }
     };
   },
-  multiValue: (styles, { data }) => {
-    const color = data.color;
-    return {
-      ...styles,
-      backgroundColor: colors.shallowPink,
-      maxWidth: '116px'
-    };
-  },
+  valueContainer: (styles) => ({
+    ...styles,
+    '& > div:first-child': {
+      maxWidth: 'fit-content'
+    }
+  }),
+  multiValue: (styles) => ({
+    ...styles,
+    backgroundColor: colors.shallowPink,
+    maxWidth: '99px'
+  }),
   multiValueLabel: (styles, { data }) => ({
     ...styles,
     color: data.color
