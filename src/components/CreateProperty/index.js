f6dd0a12ea2df9dab69b80dc472c5e40be5077bc
Carlos Rodriguez <carlos15erh@gmail.com>
2022-10-06 11:50:03 -0500

    Add Create Property component. not working

---
+++ src/components/CreateProperty/index.js
@@ -0,0 +1,101 @@
+import Button from '../Button';
+import Choice from '../Choice';
+import Check from '../Inputs/Check';
+import Number from '../Inputs/Number';
+import TextArea from '../Inputs/TextArea';
+import Search from '../Search';
+import Select from '../Select';
+import UploadImage from '../UploadImage';
+import {
+  CreatePropertyWrapper,
+  CrPrForm,
+  CrPrPets,
+  CrPrPhotos,
+  CrPrPhPreviewEmpty,
+  CrPrProperty,
+  CrPrSpaces
+} from './styles';
+
+const CHOICE_OPTIONS = [
+  { value: 'rent', label: 'Rent' },
+  { value: 'sale', label: 'Sale' }
+];
+
+const SPACE_OPTIONS = [
+  { value: '1', label: '1' },
+  { value: '2', label: '2' },
+  { value: '3', label: '3' },
+  { value: '4', label: '4' },
+  { value: '5', label: '5' }
+];
+
+const PETS_CAPTION =
+  'Allowing pets increases the likehood of renters  liking the property by 9001.\nIt also makes you a better person.';
+
+const ABOUT_CAPTION =
+  'Renters will read this first, so highlight any features or important information the apartment has.';
+
+function CreateProperty() {
+  return (
+    <CreatePropertyWrapper>
+      <CrPrForm>
+        <h1>Create a Property listing</h1>
+        <Choice options={CHOICE_OPTIONS} label='operation type' />
+        <Search placeholder='start typing yo autocomplete' label={'Address'} />
+        <Number label={'price'} placeholder={'2000'} />
+        <CrPrProperty>
+          <p>property type</p>
+          <div>
+            <Check id={'apartment'} value='Apartment' name='aparment' />
+            <Check id={'apartment'} value='House' name='house' />
+          </div>
+        </CrPrProperty>
+        <CrPrSpaces>
+          <div>
+            <Select labelValue={'Bedrooms'} options={SPACE_OPTIONS} />
+          </div>
+          <div>
+            <Select labelValue={'Bathrooms'} options={SPACE_OPTIONS} />
+          </div>
+          <div>
+            <Number label={'area in m2'} icon={false} />
+          </div>
+        </CrPrSpaces>
+        <CrPrPets>
+          <Check value='Pets Allowed' />
+          <p>{PETS_CAPTION}</p>
+        </CrPrPets>
+
+        <TextArea
+          label='about this property'
+          placeholder={'My appartment is great because...'}
+          caption={ABOUT_CAPTION}
+        />
+
+        <CrPrPhotos>
+          <h3>Photos</h3>
+          <div>
+            <UploadImage
+              captionText={'Only images, max 5MB'}
+              labelValue={'upload as many photos as you wish'}
+            />
+          </div>
+          <div>
+            <CrPrPhPreviewEmpty>
+              <p>No photos yet</p>
+            </CrPrPhPreviewEmpty>
+          </div>
+        </CrPrPhotos>
+        <div>
+          <Button
+            size='large'
+            type='primary'
+            value={'publish property listing'}
+          />
+        </div>
+      </CrPrForm>
+    </CreatePropertyWrapper>
+  );
+}
+
+export default CreateProperty;
---
+++ src/components/CreateProperty/styles.js
@@ -0,0 +1,85 @@
+import styled from '@emotion/styled';
+import { colors } from '../../styles/colors';
+import { typography } from '../../styles/typography';
+
+export const CreatePropertyWrapper = styled.div`
+  padding: 0 12rem;
+`;
+
+export const CrPrForm = styled.form`
+  padding: 3.2rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1.6rem;
+  & h1 {
+    ${typography.headline.l};
+  }
+`;
+
+export const CrPrProperty = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.4rem;
+  & p {
+    ${typography.overline};
+  }
+  & div {
+    display: flex;
+    gap: 1.6rem;
+    padding: 0.8rem 0;
+  }
+`;
+
+export const CrPrSpaces = styled.div`
+  display: flex;
+  flex-wrap: nowrap;
+  gap: 1.6rem;
+`;
+
+export const CrPrPets = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.4rem;
+  & p {
+    white-space: pre;
+    ${typography.caption};
+    color: ${colors.gray};
+  }
+`;
+
+export const CrPrPhotos = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.8rem;
+  h3 {
+    ${typography.headline.xs};
+  }
+  & div {
+    &:nth-of-type(1) {
+      display: flex;
+      flex-direction: column;
+      gap: 1.6rem;
+    }
+    &:nth-of-type(2) {
+      background-color: #f5f5f6;
+      width: 100%;
+      max-width: 60rem;
+      height: 136px;
+      padding: 0.8rem;
+    }
+  }
+`;
+
+export const CrPrPhPreviewEmpty = styled.div`
+  background-color: ${colors.backgroundLight};
+  border-radius: 0.8rem;
+  width: 12rem;
+  height: 12rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  & p {
+    ${typography.caption};
+    color: ${colors.gray};
+  }
+`;
