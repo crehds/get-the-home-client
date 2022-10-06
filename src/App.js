5f9f3cc43a625b4aeaa4549807e63207f7660340
Carlos Rodriguez <carlos15erh@gmail.com>
2022-10-06 09:20:40 -0500

    App with Routes v1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
+import CreateProperty from './components/CreateProperty';
+
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <div>NavBar</div>
+      <Routes>
+        <Route index element={<div>Landing</div>} />
+        <Route path='/create-property' element={<CreateProperty />} />
+      </Routes>
+      <div>Footer</div>
+    </>
+  );
 }
 
 export default App;
