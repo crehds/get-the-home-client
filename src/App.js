f8e2223bcf41f04340821ed1a170c84995c71f49
Carlos Rodriguez <carlos15erh@gmail.com>
2022-10-07 01:26:34 -0500

    Add: Main container to app. handle by location

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import CreateProperty from './components/CreateProperty';
+import Main from './containers/Main';
 
 function App() {
+  const location = useLocation();
   return (
     <>
       <div>NavBar</div>
-      <Routes>
-        <Route index element={<div>Landing</div>} />
-        <Route path='/create-property' element={<CreateProperty />} />
-      </Routes>
+      <Main location={location}>
+        <Routes>
+          <Route index element={<div>Landing</div>} />
+          <Route path='/create-property' element={<CreateProperty />} />
+          <Route
+            path='/property-detail'
+            element={<div>Detalles de propiedad</div>}
+          />
+        </Routes>
+      </Main>
       <div>Footer</div>
     </>
   );
