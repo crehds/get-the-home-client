6e91c8b4ff2f8c67de67bafdec68f3dd7d14c50b
Carlos Rodriguez <carlos15erh@gmail.com>
2022-10-08 12:18:09 -0500

    Add render icon of Button by side

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -7,14 +7,15 @@ const BUTTONS = {
   large: <LargeButton />
 };
 
-function Button({ size, type = '', value, icon, onClick }) {
+function Button({ size, type = '', value, icon, onClick, sideIcon = 'left' }) {
   return cloneElement(BUTTONS[size], {
     onClick,
     className: type,
     children: (
       <>
-        {icon}
+        {sideIcon === 'left' && icon}
         {value}
+        {sideIcon === 'right' && icon}
       </>
     )
   });
