a4703028a2ee39309dc41cfa510b67694ef5219c
Carlos Rodriguez <carlos15erh@gmail.com>
2022-10-10 23:14:53 -0500

    Fix:  clean warnings of console

--- src/components/ListView/index.js
+++ src/components/ListView/index.js
@@ -11,9 +11,9 @@ import { ListViewWrapper, PropertiesFound } from './styles';
 const initialState = {
   price: { min: 0, max: Infinity },
   property_type: { apartment: false, house: false },
-  pets: false, 
+  pets: false,
   area: { min: 0, max: Infinity },
-  operation_type: ""
+  operation_type: ''
 };
 function ListView() {
   // let slides = [];
@@ -29,32 +29,18 @@ function ListView() {
           data.map((e) => ({
             ...e,
             photo:
-              'https://dnm.nflximg.net/api/v6/BvVbc2Wxr2w6QuoANoSpJKEIWjQ/AAAAQZUkwT6XhdDnNqAsPrZiQWWHvhpJo0cviRndWweNeFE0G6sOOa7ltzrwXSocCIsqRqAcruHZtEk-MBx_qLAJz-43yAbJAJXmEYKEMD78GRjJ3ro5x5T97jaAj0NwMiaHvO4mNGLRmwNAPE2yA0LWWV1UfQI.jpg?r=48b',
+              'https://dnm.nflximg.net/api/v6/BvVbc2Wxr2w6QuoANoSpJKEIWjQ/AAAAQZUkwT6XhdDnNqAsPrZiQWWHvhpJo0cviRndWweNeFE0G6sOOa7ltzrwXSocCIsqRqAcruHZtEk-MBx_qLAJz-43yAbJAJXmEYKEMD78GRjJ3ro5x5T97jaAj0NwMiaHvO4mNGLRmwNAPE2yA0LWWV1UfQI.jpg?r=48b'
           }))
         );
       })
       .then(() => setRenderProperties(properties))
       .catch(console.log);
   }, []);
-  // useEffect(() => {
-  //   const newProperties = properties.filter(
-  //     (property) =>
-  //       property.price >= filters.price['min'] &&
-  //       property.price <= filters.price['max']
-  //   );
-  //   console.log(newProperties);
-  //   // newProperties.length > 0 && setRenderProperties(newProperties);
-  //   (filters.price.min === 0 && filters.price.max === Infinity || filters.price.min === "" && filters.price.max === "")? setRenderProperties(properties) : setRenderProperties(newProperties);
-  //   // newProperties.length === 0 && setRenderProperties([]);
-  //   // newProperti;
-  // }, [filters]);
-
-  console.log(filters);
 
   function filterByRange(properties, range, attribute) {
     if (range.min === 0 && range.max === Infinity) return properties;
     if (range.min === '' && range.max === '') return properties;
-    if (attribute === "price") {
+    if (attribute === 'price') {
       if (range.min === '')
         return properties.filter((property) => property.price <= range.max);
       if (range.max === '')
@@ -65,13 +51,13 @@ function ListView() {
       );
     } else {
       if (range.min === '')
-      return properties.filter((property) => property.area <= range.max);
+        return properties.filter((property) => property.area <= range.max);
       if (range.max === '')
         return properties.filter((property) => property.area >= range.min);
 
       return properties.filter(
         (property) => property.area >= range.min && property.area <= range.max
-    );
+      );
     }
   }
 
@@ -93,27 +79,25 @@ function ListView() {
   }
 
   function filterByPets(properties, pets) {
-    if (pets === false ) return properties;
-    console.log(properties)
+    if (pets === false) return properties;
+    console.log(properties);
 
-     if (pets === true) {
-      return properties.filter(
-        (property) => property.pets === true
-      );
+    if (pets === true) {
+      return properties.filter((property) => property.pets === true);
     }
   }
 
-  const propertiesByPrice = filterByRange(properties, filters.price, "price");
+  const propertiesByPrice = filterByRange(properties, filters.price, 'price');
   const propertiesByPropertyType = filterByPropertyType(
     propertiesByPrice,
     filters.property_type
   );
-  const propertiesByPets = filterByPets(
-    propertiesByPropertyType,
-    filters.pets
+  const propertiesByPets = filterByPets(propertiesByPropertyType, filters.pets);
+  const propertiesByRange = filterByRange(
+    propertiesByPets,
+    filters.area,
+    'area'
   );
-  const propertiesByRange = filterByRange(propertiesByPets, filters.area, "area");
-
 
   return (
     <Section>
