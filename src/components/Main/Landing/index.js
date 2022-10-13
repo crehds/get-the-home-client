1beaf5f620b91d07d189d0199bf3ad779b4e1f58
Rodrigo López <rodrigo.lopez.160795@hotmail.com>
2022-10-12 20:40:36 -0500

    Add the first 3 properties in Landing page

--- src/components/Main/Landing/index.js
+++ src/components/Main/Landing/index.js
@@ -1,4 +1,6 @@
+import { useEffect, useState } from 'react';
 import landingIlustration from '../../../assets/images/landingIlustration.svg';
+import { getProperties } from '../../../services/unlogged-service';
 import Button from '../../Button';
 import DeveloperCard from '../../DeveloperCard';
 import PropertyCard from '../../PropertyCard';
@@ -8,60 +10,29 @@ import {
   WrapperProducts,
   WrapperSearch,
   WrapperSignup,
-  WrapperTeam
+  WrapperTeam,
 } from './styles';
 
-const cards = [
-  {
-    id: 1,
-    operationType: 'sale',
-    photo:
-      'https://dnm.nflximg.net/api/v6/BvVbc2Wxr2w6QuoANoSpJKEIWjQ/AAAAQZUkwT6XhdDnNqAsPrZiQWWHvhpJo0cviRndWweNeFE0G6sOOa7ltzrwXSocCIsqRqAcruHZtEk-MBx_qLAJz-43yAbJAJXmEYKEMD78GRjJ3ro5x5T97jaAj0NwMiaHvO4mNGLRmwNAPE2yA0LWWV1UfQI.jpg?r=48b',
-    price: 30000,
-    propertyType: 'Apartment',
-    address: '86872 Jacob Gateway, Durganport, WV 48044',
-    bedrooms: 4,
-    bathrooms: 2,
-    area: 180,
-    pet: false,
-    favorite: false
-  },
-  {
-    id: 2,
-    operationType: 'sale',
-    photo:
-      'https://dnm.nflximg.net/api/v6/BvVbc2Wxr2w6QuoANoSpJKEIWjQ/AAAAQZUkwT6XhdDnNqAsPrZiQWWHvhpJo0cviRndWweNeFE0G6sOOa7ltzrwXSocCIsqRqAcruHZtEk-MBx_qLAJz-43yAbJAJXmEYKEMD78GRjJ3ro5x5T97jaAj0NwMiaHvO4mNGLRmwNAPE2yA0LWWV1UfQI.jpg?r=48b',
-    price: 3000,
-    propertyType: 'Apartment',
-    address: '86872 Jacob Gateway, Durganport, WV 48044',
-    bedrooms: 4,
-    bathrooms: 2,
-    area: 1800,
-    pet: true,
-    favorite: false
-  },
-  {
-    id: 3,
-    operationType: 'rent',
-    photo:
-      'https://dnm.nflximg.net/api/v6/BvVbc2Wxr2w6QuoANoSpJKEIWjQ/AAAAQZUkwT6XhdDnNqAsPrZiQWWHvhpJo0cviRndWweNeFE0G6sOOa7ltzrwXSocCIsqRqAcruHZtEk-MBx_qLAJz-43yAbJAJXmEYKEMD78GRjJ3ro5x5T97jaAj0NwMiaHvO4mNGLRmwNAPE2yA0LWWV1UfQI.jpg?r=48b',
-    price: 30000,
-    propertyType: 'Apartment',
-    address: '86872 Jacob Gateway, Durganport, WV 48044',
-    bedrooms: 4,
-    bathrooms: 2,
-    area: 20,
-    pet: true,
-    favorite: false
-  }
-];
-
 function Landing() {
+  const [properties, setProperties] = useState([]);
+  useEffect(() => {
+    getProperties()
+      .then((data) => {
+        setProperties(
+          data.map((e) => ({
+            ...e,
+            photo:
+              'https://dnm.nflximg.net/api/v6/BvVbc2Wxr2w6QuoANoSpJKEIWjQ/AAAAQZUkwT6XhdDnNqAsPrZiQWWHvhpJo0cviRndWweNeFE0G6sOOa7ltzrwXSocCIsqRqAcruHZtEk-MBx_qLAJz-43yAbJAJXmEYKEMD78GRjJ3ro5x5T97jaAj0NwMiaHvO4mNGLRmwNAPE2yA0LWWV1UfQI.jpg?r=48b',
+          }))
+        );
+      })
+      .catch(console.log);
+  }, []);
   return (
     <WrapperMain>
       <WrapperSearch>
-        <img src={landingIlustration} alt="landing ilustration" />
-        <img src={landingIlustration} alt="landing ilustration" />
+        <img src={landingIlustration} alt='landing ilustration' />
+        <img src={landingIlustration} alt='landing ilustration' />
         <div>
           <h1>Meet your new Home</h1>
           <p>The easiest way to find where you belong</p>
@@ -71,21 +42,20 @@ function Landing() {
         <p>Find an Apartment you Love</p>
         <h4>Homes for rent at the best prices</h4>
         <div>
-          {cards.map((card) => {
+          {properties.slice(0, 3).map((property) => {
             return (
               <PropertyCard
-                key={card.id}
-                operationType={card.operationType}
-                photo={card.photo}
-                price={card.price.toLocaleString('en-US')}
-                propertyType={card.propertyType}
-                address={card.address}
-                bedrooms={card.bedrooms}
-                bathrooms={card.bathrooms}
-                area={card.area}
-                pet={card.pet}
-                favorite={card.favorite}
-                active={card.active}
+                key={property.id}
+                operationType={property.operation_type}
+                photo={property.photo}
+                price={property.price.toLocaleString('en-US')}
+                propertyType={property.property_type}
+                address={property.address}
+                bedrooms={property.bedrooms}
+                bathrooms={property.bathrooms}
+                area={property.area}
+                pet={property.pets}
+                active={property.active}
               ></PropertyCard>
             );
           })}
@@ -97,8 +67,8 @@ function Landing() {
             Getting someone to rent your apartment has never been this easy
           </h4>
           <Button
-            size="large"
-            type="primary"
+            size='large'
+            type='primary'
             value={'Create an account now'}
           ></Button>
         </div>
