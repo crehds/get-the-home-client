7adb51d2b3ac437dfe5f81161a9d67fb916c96f8
Rodrigo López <rodrigo.lopez.160795@hotmail.com>
2022-10-12 20:50:07 -0500

    Send properties info to property card in carousel

--- src/components/ListView/Carousel/index.js
+++ src/components/ListView/Carousel/index.js
@@ -9,7 +9,19 @@ function Carousel({ slides }) {
       {newSlides.map((cards, i) => (
         <Wrapper key={`list-view-slide-${i}`}>
           {cards.map((card, j) => (
-            <PropertyCard key={`list-view-card-${i}-${j}`} {...card} />
+            <PropertyCard
+              key={`list-view-card-${i}-${j}`}
+              operationType={card.operation_type}
+              photo={card.photo}
+              price={card.price.toLocaleString('en-US')}
+              propertyType={card.property_type}
+              address={card.address}
+              bedrooms={card.bedrooms}
+              bathrooms={card.bathrooms}
+              area={card.area}
+              pet={card.pets}
+              active={card.active}
+            />
           ))}
         </Wrapper>
       ))}
