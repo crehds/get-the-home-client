6e39bf1077b0716f3bc045cf1a1443af14249be0
Rodrigo López <rodrigo.lopez.160795@hotmail.com>
2022-11-02 16:15:11 -0600

    Add type on inputy

--- src/components/Inputs/Input/index.js
+++ src/components/Inputs/Input/index.js
@@ -1,6 +1,15 @@
 import { Caption, InputWrapper, Label, StyledInput } from './styles';
 
-function Input({ label, caption, placeholder, onChange, onBlur, value, name }) {
+function Input({
+  label,
+  caption,
+  placeholder,
+  onChange,
+  onBlur,
+  value,
+  name,
+  type = 'text',
+}) {
   return (
     <InputWrapper>
       {label && <Label>{label}</Label>}
@@ -10,6 +19,7 @@ function Input({ label, caption, placeholder, onChange, onBlur, value, name }) {
         onBlur={onBlur}
         value={value}
         name={name}
+        type={type}
       />
       {caption && <Caption>{caption}</Caption>}
     </InputWrapper>
