45825b178e512d74d1905f2650f057568fda7abb
Mario Zuniga <mmzv999@gmail.com>
2022-11-13 18:31:11 -0500

    Fix savedproperties view

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-export const BASE_URI = 'http://127.0.0.1:3000';
-// export const BASE_URI =
-//   'https://get-that-home-api-production-6f10.up.railway.app';
+// export const BASE_URI = 'http://127.0.0.1:3000';
+export const BASE_URI =
+  'https://get-that-home-api-production-6f10.up.railway.app';
 export const tokenKey = 'getthathome-token';
 export const userKey = 'get-the-home-user';
