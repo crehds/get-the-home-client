217cf0ca20a66567547b0c204b4ba5284d1e6463
Carlos Rodriguez <carlos15erh@gmail.com>
2022-11-13 14:40:34 -0500

    Update: handle map by property address

--- src/components/PropertyDetail/Info/index.js
+++ src/components/PropertyDetail/Info/index.js
@@ -2,7 +2,7 @@ import Carousel from 'nuka-carousel';
 import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
 import { RiMoneyDollarCircleLine } from 'react-icons/ri';
 import { BiBed, BiBath, BiArea } from 'react-icons/bi';
-import { MdPets } from 'react-icons/md'
+import { MdPets } from 'react-icons/md';
 import {
   AboutDetails,
   BuildingDetails,
@@ -21,17 +21,15 @@ import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { showProperty } from '../../../services/unlogged-service';
 
-
 const images = [property1, property2, property3, property4];
 function Info() {
   const [property, setProperty] = useState('');
   let { id } = useParams();
 
-
   useEffect(() => {
     showProperty(id).then(setProperty).catch(console.log);
   }, [id]);
-  const commaSeparator = property.address && property.address.indexOf(',')
+  const commaSeparator = property.address && property.address.indexOf(',');
 
   return (
     <InfoWrapper>
@@ -69,7 +67,11 @@ function Info() {
         <GeneralDetails>
           <div>
             <h4>{property.address && property.address.split(',')[0]}</h4>
-            <p>{commaSeparator !==-1 && property.address && property.address.slice(commaSeparator + 1)}</p> 
+            <p>
+              {commaSeparator !== -1 &&
+                property.address &&
+                property.address.slice(commaSeparator + 1)}
+            </p>
           </div>
           <div>
             <h4>
@@ -82,19 +84,19 @@ function Info() {
         <BuildingDetails>
           <div>
             <BiBed size={32} color={colors.gray} />
-            <p>{property.bedrooms && property.bedrooms + " bedrooms"}</p>
+            <p>{property.bedrooms && property.bedrooms + ' bedrooms'}</p>
           </div>
           <div>
             <BiBath size={32} color={colors.gray} />
-            <p>{property.bathrooms && property.bathrooms + " bathrooms"} </p>
+            <p>{property.bathrooms && property.bathrooms + ' bathrooms'} </p>
           </div>
           <div>
             <BiArea size={32} color={colors.gray} />
-            <p>{property.area && property.area + " m2"} </p>
+            <p>{property.area && property.area + ' m2'} </p>
           </div>
           <div>
             <MdPets size={32} color={colors.gray} />
-            <p>{property.pets? "Pets allowed": "Pets not allowed"}</p>
+            <p>{property.pets ? 'Pets allowed' : 'Pets not allowed'}</p>
           </div>
         </BuildingDetails>
         <AboutDetails>
@@ -105,9 +107,21 @@ function Info() {
           <h6>Location</h6>
           <p>{property.address}</p>
           <div>
-            <iframe
+            {/* <iframe
               title='google-map'
               src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1950.8829997656744!2d-77.03898822064161!3d-12.059614858222544!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c8c1658a33b1%3A0x44b362557e937378!2sAv.%209%20de%20Diciembre%20236%2C%20Lima%2015001!5e0!3m2!1ses!2spe!4v1665153862081!5m2!1ses!2spe'
+              width='600'
+              height='450'
+              style={{ border: 0 }}
+              allowFullScreen=''
+              loading='lazy'
+              referrerPolicy='no-referrer-when-downgrade'
+            ></iframe> */}
+            <iframe
+              title='google-map'
+              src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyAWknVh_Fg0GBYYAjcDXsPjTZPnCEV8nJ4&q=${property.address
+                ?.split(' ')
+                .join('+')}`}
               width='600'
               height='450'
               style={{ border: 0 }}
